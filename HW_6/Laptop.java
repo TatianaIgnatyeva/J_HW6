d63bc7d7f5e5528a5d3caa40cd6aca1c48219569
TatianaIgnatyeva <121223547+TatianaIgnatyeva@users.noreply.github.com>
2023-06-09 22:32:59 +0300

    Add files via upload

---
+++ HW_6/Laptop.java
@@ -0,0 +1,33 @@
+import java.util.*;
+/**
+ * Подумать над структурой класса Ноутбук для магазина техники - выделить поля и
+ * методы. Реализовать в java.
+ * Создать множество ноутбуков.
+ * Написать метод, который будет запрашивать у пользователя критерий (или
+ * критерии) фильтрации и выведет ноутбуки,
+ * отвечающие фильтру. 
+
+public class Laptop {
+    int id;
+    int ram;
+    int ssd;
+    String os;
+    String color;
+
+    public Laptop(int id, int ram, int ssd, String os, String color){
+        this.id = id;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.os = os;
+        this.color = color;
+    }
+
+    public String toString() {
+        return String.format("id: %d, ram: %d ГБ, ssd: %d ГБ, OS: %s, color: %s", id, ram, ssd, os, color);
+    }
+
+    public boolean equals(Object o) {
+        Laptop t = (Laptop) o;
+        return id == t.id;
+    }
+}
